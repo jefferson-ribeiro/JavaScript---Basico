ec8d6653cfa18d9faa4060263d7e7bdc6173b3f2
Jefferson Augusto Ribeiro <jeffersonari@yahoo.com.br>
2020-11-29 18:26:33 -0300

    Update repeticao.js

--- repeticao.js
+++ repeticao.js
@@ -1,7 +1,14 @@
 var numeroSorteado = 10;
 
-var tabuada = 7;
+//var tabuada = 7;
 
-for (var i = 0; i < 10; i++) {
-    console.log("valor de " + tabuada + " x " + i + " = " + tabuada * i);
+//for (var i = 0; i < 10; i++) {
+//    console.log("valor de " + tabuada + " x " + i + " = " + tabuada * i);
+//}
+
+for (var i = 0; i < 100; i++) {
+    if (numeroSorteado === i) {
+        console.log ("seu número foi encontrado");
+        break;
+    }
 }
